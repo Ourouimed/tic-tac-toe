10f13983a6ad2f81727fc01aa32c172e09319a1d
Mohamed Amine Ouroui <166921147+Ourouimed@users.noreply.github.com>
2024-11-21 22:38:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,50 @@
 let squares = document.querySelectorAll(".square")
 let title = document.querySelector(".title")
-let current = "X"
 let reset = document.querySelector("#reset")
+let current = "X"
 
-
-// Reset Game After win 
-const ResetAfterWin = ()=>{
-    setTimeout(()=>{
-        setInterval(()=> title.innerHTML += ".",1000)
-        title.innerHTML = "Wait 3 secs"
-        setTimeout(()=> location.reload(),3000)
-    },1000)
-}
-
+// End Game After win 
 const EndGame = (a , b , c)=>{
     title.innerHTML = `${squares[a].innerHTML} wins `
     squares[a].classList.add("checked")
     squares[b].classList.add("checked")
     squares[c].classList.add("checked")
+    setTimeout(()=>{
+        title.innerHTML = "Wait 3 secs"
+        setInterval(()=> title.innerHTML += ".",1000)
+        setTimeout(()=> location.reload(),3000)
+    },1000)
 }
 // Winner 
 const Xowinner = ()=>{
     // Vertical win
     if(squares[0].innerHTML == squares[1].innerHTML && squares[1].innerHTML == squares[2].innerHTML && squares[0].innerHTML !=  ''){
         EndGame(0,1,2)
-        ResetAfterWin()
     }   
     else if (squares[3].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[5].innerHTML && squares[3].innerHTML){
         EndGame(3,4,5)
-        ResetAfterWin()
     }
     else if (squares[6].innerHTML == squares[7].innerHTML && squares[7].innerHTML == squares[8].innerHTML && squares[6].innerHTML){
         EndGame(6,7,8)
-        ResetAfterWin()
     }
 
     // Horizontal win
     else if (squares[0].innerHTML == squares[3].innerHTML && squares[3].innerHTML == squares[6].innerHTML && squares[6].innerHTML){
         EndGame(0,3,6)
-        ResetAfterWin()
     }
     else if (squares[1].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[7].innerHTML && squares[1].innerHTML){
         EndGame(1,4,7)
-        ResetAfterWin()
     }
     else if (squares[2].innerHTML == squares[5].innerHTML && squares[5].innerHTML == squares[8].innerHTML && squares[2].innerHTML){
         EndGame(2,5,8)
-        ResetAfterWin()
     }
 
     // Axes win
     else if (squares[0].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[8].innerHTML && squares[0].innerHTML){
         EndGame(0,4,8)
-        ResetAfterWin()
     }
     else if (squares[2].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[6].innerHTML && squares[2].innerHTML){
         EndGame(2,4,6)
-        ResetAfterWin()
     }
 }
 
